d52bf4fd91c159e96618128f3c7d2bc7f3a2b094
Mehul <318097@gmail.com>
2020-02-06 22:44:37 +0530

    Updates

---
+++ precache-manifest.cb2347af08273c8c7aaf66f7c1404c29.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e430d100adb30e63d15dcc640cb93bcd",
+    "url": "/store-calculator/index.html"
+  },
+  {
+    "revision": "9728e1f54f360896d4b6",
+    "url": "/store-calculator/static/css/2.aef8ad04.chunk.css"
+  },
+  {
+    "revision": "55ce7504d53bef871b53",
+    "url": "/store-calculator/static/css/main.3f9d09de.chunk.css"
+  },
+  {
+    "revision": "9728e1f54f360896d4b6",
+    "url": "/store-calculator/static/js/2.79dea351.chunk.js"
+  },
+  {
+    "revision": "b651d5533fc80509587379dd07cc06bd",
+    "url": "/store-calculator/static/js/2.79dea351.chunk.js.LICENSE"
+  },
+  {
+    "revision": "55ce7504d53bef871b53",
+    "url": "/store-calculator/static/js/main.a23b5e51.chunk.js"
+  },
+  {
+    "revision": "635c64de2317b4e9f9aa",
+    "url": "/store-calculator/static/js/runtime-main.4b37afb5.js"
+  }
+]);
